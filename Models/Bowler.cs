2cb9b29c54883c842882b91455158f73d2979794
bennlee01 <153874824+bennlee01@users.noreply.github.com>
2025-03-18 14:11:24 -0600

    Update Bowler.cs

--- Models/Bowler.cs
+++ Models/Bowler.cs
@@ -11,28 +11,28 @@ namespace BowlingLeagueAPI.Models
         public int BowlerID { get; set; }
 
         // Bowler's first name, defaults to an empty string if not provided
-        public string BowlerFirstName { get; set; } = string.Empty;
+        public string? BowlerFirstName { get; set; } = string.Empty;
 
         // Bowler's middle initial, defaults to an empty string if not provided
-        public string BowlerMiddleInit { get; set; } = string.Empty;
+        public string? BowlerMiddleInit { get; set; } = string.Empty;
 
         // Bowler's last name, defaults to an empty string if not provided
-        public string BowlerLastName { get; set; } = string.Empty;
+        public string? BowlerLastName { get; set; } = string.Empty;
 
         // Bowler's home address, defaults to an empty string if not provided
-        public string BowlerAddress { get; set; } = string.Empty;
+        public string? BowlerAddress { get; set; } = string.Empty;
 
         // Bowler's city, defaults to an empty string if not provided
-        public string BowlerCity { get; set; } = string.Empty;
+        public string? BowlerCity { get; set; } = string.Empty;
 
         // Bowler's state, defaults to an empty string if not provided
-        public string BowlerState { get; set; } = string.Empty;
+        public string? BowlerState { get; set; } = string.Empty;
 
         // Bowler's zip code, defaults to an empty string if not provided
-        public string BowlerZip { get; set; } = string.Empty;
+        public string? BowlerZip { get; set; } = string.Empty;
 
         // Bowler's phone number, defaults to an empty string if not provided
-        public string BowlerPhoneNumber { get; set; } = string.Empty;
+        public string? BowlerPhoneNumber { get; set; } = string.Empty;
 
         // Foreign key to associate the Bowler with a Team
         public int TeamID { get; set; }
